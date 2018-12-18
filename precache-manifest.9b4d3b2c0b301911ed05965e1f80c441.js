530b5ea8f095e5a7918be740959ac5c13fcec1ca
Cat <madcatmac16@gmail.com>
2018-12-17 18:30:37 -0700

    Updates

---
+++ precache-manifest.9b4d3b2c0b301911ed05965e1f80c441.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4d38c53c571e29041da",
+    "url": "/memory-click-game/static/js/runtime~main.f4d38c53.js"
+  },
+  {
+    "revision": "181365635bf0b7bb7063",
+    "url": "/memory-click-game/static/js/main.18136563.chunk.js"
+  },
+  {
+    "revision": "8c7c02bfc24ce582f612",
+    "url": "/memory-click-game/static/js/1.8c7c02bf.chunk.js"
+  },
+  {
+    "revision": "181365635bf0b7bb7063",
+    "url": "/memory-click-game/static/css/main.87ee03fc.chunk.css"
+  },
+  {
+    "revision": "b9249995c2dd44e0d32f05bd89e4ecca",
+    "url": "/memory-click-game/index.html"
+  }
+];
